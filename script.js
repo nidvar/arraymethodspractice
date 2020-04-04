2d339451124bd51477ddfe9c929a4eb4c757b329
Jay Kim <djayshine87@hotmail.com>
2020-04-04 19:29:53 +1100

    add user function along with fetch api

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log('array methods')
+
+const people=[]
+
+const grab_data=()=>{
+    fetch('https://randomuser.me/api')
+    .then(data=>data.json())
+    .then(data=>{
+        console.log(data.results);
+        console.log(data.results[0].name.first +' '+ data.results[0].name.last)
+        people.push({
+            name: data.results[0].name.first +' '+ data.results[0].name.last,
+            wealth: Math.floor(Math.random()*1000000)
+        })
+        document.querySelector('.screen').innerHTML = ''
+        people.forEach(a=>{
+            add_user(a.name)
+        })
+    })
+    .catch(error=>console.log(error))
+}
+
+const add_user=(name)=>{
+    const person = document.createElement('p');
+    person.textContent = name
+    document.querySelector('.screen').appendChild(person)
+}
+
+document.querySelector('.add_user').addEventListener('click',()=>{
+    grab_data()
+})
