f89543b883c9af053b4e4736d7a38b0a16818741
Jay Kim <djayshine87@hotmail.com>
2020-04-04 20:50:00 +1100

    double function

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ const grab_data=()=>{
     fetch('https://randomuser.me/api')
     .then(data=>data.json())
     .then(data=>{
-        console.log(data.results);
-        console.log(data.results[0].name.first +' '+ data.results[0].name.last)
         people.push({
             name: data.results[0].name.first +' '+ data.results[0].name.last,
             wealth: Math.floor(Math.random()*1000000)
@@ -20,6 +18,17 @@ const grab_data=()=>{
     .catch(error=>console.log(error))
 }
 
+const double =()=>{
+    let x = people.map(a=>{
+        return {name: a.name, wealth: a.wealth*2}
+    })
+    console.log(x)
+    document.querySelector('.screen').innerHTML = ''
+    x.forEach(a=>{
+        add_user(a.name, a.wealth)
+    })
+}
+
 const add_user=(name, wealth)=>{
     const person = document.createElement('p');
     person.textContent = name+': ' + ' ' + '$'+wealth
@@ -28,4 +37,12 @@ const add_user=(name, wealth)=>{
 
 document.querySelector('.add_user').addEventListener('click',()=>{
     grab_data()
+})
+
+document.querySelector('.double').addEventListener('click',()=>{
+    if(people.length===0){
+        alert('No users')
+        return
+    }
+    double();
 })
